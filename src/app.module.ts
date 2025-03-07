de7f5f53af2b29546b5c4cc3eb2379a7a9fb741f
Matthew Hunter <matthewhunter404@gmail.com>
2025-03-07 23:24:19 +0200

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configService } from './config/config';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     UsersModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
   ],
   controllers: [AppController],
   providers: [AppService],
