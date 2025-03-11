08df334830b6977ed41140866d12208f5c7893d8
Matthew Hunter <matthewhunter404@gmail.com>
2025-03-11 13:59:56 +0200

    Add custom config export
    Replace process.env reading in main.ts with ConfigService usage

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import typeORMConfig from './config/typeorm.config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import config from './config/config';
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { StructuresModule } from './structures/structures.module';
 import { RolesModule } from './roles/roles.module';
+
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule.forRoot({
-      envFilePath: ['.env'],
+      load: [config],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { throwError } from 'rxjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
         transform: true,
     }),
 );
-  await app.listen(process.env.PORT ?? 3000);
+  const appPort = configService.get('port');
+  console.log("appPort: "+appPort)
+  await app.listen(appPort);
 }
 bootstrap();
